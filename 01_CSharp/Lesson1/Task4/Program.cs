a27d32a08b1f5ad4f30ca3adaea8789d81d7a142
Vladislav Kuznetsov <kuznetsovx13@yandex.ru>
2024-01-19 23:48:20 +0300

    add lesson1:task4

---
+++ 01_CSharp/Lesson1/Task4/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 4
+// Даны две целых числа. Вывести на экран большее из них
+
+// В каталог вставил блок-схему для решения данной задачи (Flowchart_4.png)
+
+// Решение
+
+int a = 1;
+int b = 7;
+int c = 9;
+int d = 4;
+int e = 10;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
